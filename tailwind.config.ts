9178cd783fc11edbf5b97a23bf84ac4fcd3cf5ce
heondeam <aoao1023@gmail.com>
2024-05-02 21:45:27 +0900

    chore: add packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,93 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
-    colors: {
-      darkNavy: "#020c1b",
-      navy: "#0a192f",
-      lightNavy: "#112240",
-      lightestNavy: "#233554",
-      navyShadow: "rgba(2, 12, 27, 0.7)",
-      darkSlate: "#495670",
-      slate: "#8892b0",
-      lightSlate: "#a8b2d1",
-      lightestSlate: "#ccd6f6",
-      white: "#e6f1ff",
-      green: "#64ffda",
-      greenTint: "rgba(100,255,218,0,1)",
-      pink: "#f57dff",
-      blue: "#57cbff",
-    },
-    borderRadius: {
-      md: "4px",
+    extend: {
+      colors: {
+        darkNavy: "var(--dark-navy)",
+        navy: "var(--navy)",
+        lightNavy: "var(--light-navy)",
+        lightestNavy: "var(--lightest-navy)",
+        darkSlate: "var(--dark-slate)",
+        slate: "var(--slate)",
+        lightSlate: "var(--light-slate)",
+        lightestSlate: "var(--lightest-slate)",
+        white: "var(--white)",
+        green: "var(--green)",
+        greenTint: "var(--green-tint)",
+        pink: "var(--pink)",
+        blue: "var(--blue)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
