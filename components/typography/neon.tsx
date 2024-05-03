891f173f833ab561558c21056cdfc84a3c45bf78
heondeam <aoao1023@gmail.com>
2024-05-03 15:28:22 +0900

    feat: add neon component

---
+++ components/typography/neon.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/utils/utils";
+import { cva } from "class-variance-authority";
+
+type NeonProps = React.HTMLAttributes<HTMLParagraphElement>;
+
+export const neonVariants = cva(
+  "whitespace-pre-line text-[20px] font-normal text-green leading-[1.5]"
+);
+
+const Neon = (props: NeonProps) => {
+  const { className, ...rest } = props;
+  return <p {...rest} className={cn(neonVariants({ className }))} />;
+};
+
+export default Neon;
