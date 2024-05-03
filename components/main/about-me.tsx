1fbe66da312a306fa0b0cb953699ac4da9a64e6f
heondeam <aoao1023@gmail.com>
2024-05-03 21:26:24 +0900

    chore: edit typo

--- components/main/about-me.tsx
+++ components/main/about-me.tsx
@@ -9,10 +9,10 @@ import Image from 'next/image'
 import Profile from '@png/profile.png'
 
 const TEXTS = [
-  'Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!',
-  'Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.',
-  'I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.',
-  'Here are a few technologies I’ve been working with recently:'
+  '안녕하세요. 2년차 프론트엔드 엔지니어 이성헌입니다. 저는 대학 동아리에서 코딩을 접한 뒤, 웹 개발에 관심을 가지게 되어 웹퍼블리셔로 커리어를 시작했고, 현재는 프론트엔드 엔지니어로 일하고 있습니다.',
+  '저는 외주 프로젝트에 참여, 정보보안관리 솔루션 회사에 근무하며 다양한 프로젝트를 경험했습니다. 이를 통해 다양한 기술을 배우고, 다양한 문제를 해결하는 방법을 배웠습니다.',
+  '일을 하며 전산학 기초지식 공부의 중요성을 느끼고, 약 5개월 간 크래프톤 정글에 입소하여 자료구조와 알고리즘, 네트워크, 운영체제 등을 공부하며 전산학 기초지식을 쌓았습니다. 이를 통해 프로그래밍에 대한 이해도를 높이고, 문제해결 능력을 키웠습니다.',
+  '저는 주로 아래와 같은 기술을 사용해서 개발합니다 :)'
 ]
 
 const SKILLS = [
@@ -27,11 +27,11 @@ const SKILLS = [
 
 export default function AboutMe() {
   return (
-    <Section className="h-screen bg-navy px-20">
+    <Section className="h-[950px] bg-navy px-20">
       <div className="max-w-[850px] flex flex-col gap-6">
         <Title title="About Me" idx="01" />
         <div className="flex justify-between gap-8">
-          <div className="flex flex-col gap-4 w-[50%]">
+          <div className="flex flex-col gap-4 w-1/2">
             {/* texts */}
             {TEXTS.map((text, idx) => (
               <P1 key={idx} className="text-slate">
@@ -39,7 +39,7 @@ export default function AboutMe() {
               </P1>
             ))}
 
-            <UnorderList items={SKILLS} />
+            <UnorderList items={SKILLS} cols={2} bulletSize="sm" />
           </div>
           <div className="image-before group">
             <div className="image-wrapper">
--- components/main/hello.tsx
+++ components/main/hello.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client'
 
-import H1 from "../typography/h1";
-import H5 from "../typography/h5";
-import P1 from "../typography/p1";
-import { Button } from "../ui/button";
-import Section from "../ui/section";
+import H1 from '../typography/h1'
+import H5 from '../typography/h5'
+import P1 from '../typography/p1'
+import { Button } from '../ui/button'
+import Section from '../ui/section'
 
 export default function Hello() {
   return (
-    <Section className="h-screen bg-navy px-4">
-      <div className="w-3/4 flex flex-col gap-6">
+    <Section className="h-screen bg-navy px-20">
+      <div className="max-w-[1000px] flex flex-col gap-6">
         <H5 className="text-green ">안녕하세요, 저는</H5>
         <H1 className="text-white">
           인상깊고 재미있는 UI/UX를 만드는
@@ -17,18 +17,16 @@ export default function Hello() {
           <span className="text-slate">프론트엔드 엔지니어 이성헌입니다.</span>
         </H1>
         <P1 className="text-slate">
-          I’m a software engineer specializing in building (and occasionally{" "}
-          <br />
-          designing) exceptional digital experiences. Currently, I’m focused on{" "}
-          <br />
+          I’m a software engineer specializing in building (and occasionally <br />
+          designing) exceptional digital experiences. Currently, I’m focused on <br />
           building accessible, human-centered products at Upstatement.
         </P1>
         <div>
           <Button className="mt-4" variant="career" size="lg">
-            Check out my career!
+            Check out my resume!
           </Button>
         </div>
       </div>
     </Section>
-  );
+  )
 }
