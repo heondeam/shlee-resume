3408591d65f7413ad5689b2f893757ac2a5601e6
heondeam <aoao1023@gmail.com>
2024-05-03 15:28:22 +0900

    feat: add neon component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutMe from "@/components/main/about-me";
 import Hello from "@/components/main/hello";
 
 export default function Home() {
   return (
     <main>
       <Hello />
+      <AboutMe />
     </main>
   );
 }
