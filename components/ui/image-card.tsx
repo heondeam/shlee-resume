39425e219f7c3dd38b9ece092bd912b1ec29b65e
heondeam <aoao1023@gmail.com>
2024-05-05 14:28:58 +0900

    feat: imple image card

---
+++ components/ui/image-card.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { cn } from '@utils/utils'
+import Image, { StaticImageData } from 'next/image'
+
+type ImageCardProps = {
+  image: StaticImageData
+  className?: string
+}
+
+export default function ImageCard({ image, className }: ImageCardProps) {
+  return (
+    <div className={cn('relative w-[600px] h-[300px] bg-green rounded-[4px]', className)}>
+      <div
+        className="absolute z-10 w-full h-full bg-slate filter grayscale brightness-75 blur-sm mix-blend-multiply 
+        group-hover:grayscale-0 group-hover:brightness-100 group-hover:blur-0 group-hover:opacity-0 transition-custom"
+      ></div>
+      <Image src={image} alt="project image" layout="fill" className="rounded-[4px]" />
+    </div>
+  )
+}
