8ae8d102d5148ed40abfd04c8ad1bb80988079d3
heondeam <aoao1023@gmail.com>
2024-05-05 22:10:22 +0900

    feat: imple worthy projects

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AboutMe from '@components/sections/about-me'
 import Experience from '@components/sections/experience'
 import Hello from '@components/sections/hello'
 import Projects from '@components/sections/projects'
+import WorthyProjects from '@components/sections/worthy-projects'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <AboutMe />
       <Experience />
       <Projects />
+      <WorthyProjects />
     </main>
   )
 }
--- components/sections/worthy-projects.tsx
+++ components/sections/worthy-projects.tsx
@@ -1,3 +1,29 @@
+'use client'
+
+import H2 from '@components/typography/h2'
+import H4 from '@components/typography/h4'
+import P1 from '@components/typography/p1'
+import Section from '@components/ui/section'
+import WorthyCard from '@components/ui/worthy-card'
+
 export default function WorthyProjects() {
-  return <></>
+  return (
+    <Section className="h-auto p-32 bg-navy px-4">
+      <div className="max-w-[950px]">
+        <div className="flex flex-col items-center gap-4">
+          <H2 className="text-white whitespace-nowrap">Other Noteworthy Projects</H2>
+          <P1 className="text-green">view the archive</P1>
+        </div>
+
+        <div className="grid grid-cols-3 mt-28 gap-[15px]">
+          <WorthyCard
+            title="Project Title"
+            description="A brief description of the project goes here."
+            stack={['React', 'TypeScript', 'TailwindCSS']}
+            link="#"
+          />
+        </div>
+      </div>
+    </Section>
+  )
 }
