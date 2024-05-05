1d317f4d31d2afba2573fa20e8fe262f3867d553
heondeam <aoao1023@gmail.com>
2024-05-05 11:11:35 +0900

    chore: set confing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
+      typescript: {
+        ignoreBuildErrors: true,
+      },
+      webpack(config) {
+        config.module.rules.push({
+          loader: "@svgr/webpack",
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: { cleanupIds: false, removeViewBox: false },
+                  },
+                },
+              ],
+            },
+            titleProp: true,
+          },
+          test: /\.svg$/,
+        })
+        return config
+      },
+};
 
 export default nextConfig;
