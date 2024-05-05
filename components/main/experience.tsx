f5a8f5ef0be657ea42b503e8a8b78deec17261cf
heondeam <aoao1023@gmail.com>
2024-05-05 15:11:20 +0900

    fix: modify experience section

--- components/main/experience.tsx
+++ components/main/experience.tsx
@@ -1,17 +1,17 @@
 'use client'
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/tabs'
-import Title from '../title/title'
+import Title from '../ui/title'
 import Section from '../ui/section'
 import H3 from '@components/typography/h3'
 import P5 from '@components/typography/p5'
-import UnorderList from '@components/list/unorder-list'
+import UnorderList from '@components/ui/unorder-list'
 
 export default function Experience() {
   return (
-    <Section className="h-[950px] bg-navy px-4">
+    <Section className="h-[550px] bg-navy px-20">
       <div className="max-w-[850px] flex flex-col gap-6">
-        <Title title="Where I've Worked" idx="02" />
+        <Title title="Work Experience" idx="02" />
         <Tabs className="flex gap-6" defaultValue="team-sparta">
           <TabsList>
             <TabsTrigger value="team-sparta">팀스파르타</TabsTrigger>
@@ -42,7 +42,7 @@ export default function Experience() {
           <TabsContent value="dsntech">
             <div className="flex flex-col gap-4">
               <div className="space-y-1">
-                <H3 className="text-white">외주팀/프리랜서 @ 팀스파르타</H3>
+                <H3 className="text-white">연구소/팀원 @ 디에스엔텍</H3>
                 <P5 className="text-slate">July - Decenber 2016</P5>
               </div>
               <UnorderList
@@ -62,12 +62,12 @@ export default function Experience() {
           <TabsContent value="the-buildsoft">
             <div className="flex flex-col gap-4">
               <div className="space-y-1">
-                <H3 className="text-white">외주팀/프리랜서 @ 팀스파르타</H3>
+                <H3 className="text-white">개발1팀/인턴 @ 더빌드소프트</H3>
                 <P5 className="text-slate">July - Decenber 2016</P5>
               </div>
               <UnorderList
                 items={[
-                  'Developed and styled interactive web applications for Apple Music using Ember and SCSS',
+                  '퍼블리셔로 입사하여, 프론트엔드 엔지니어로 성장하였고 공새로라는 프로젝트를 진행하였습니다.',
                   'Built and shipped the Apple Music Extension for Facebook Messenger leveraging third-party and internal API integrations',
                   'Architected and implemented the user interface of Apple Music"s embeddable web player widget for in-browser user authorization and full song playback',
                   'Collaborated with designers and product managers to define and implement new features and user experiences'
