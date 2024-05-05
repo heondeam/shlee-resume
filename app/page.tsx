6e16cf956a4ecf9153ec228a48655f866c75dd80
heondeam <aoao1023@gmail.com>
2024-05-05 15:10:59 +0900

    feat: imple project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import AboutMe from "@/components/main/about-me";
-import Experience from "@/components/main/experience";
-import Hello from "@/components/main/hello";
+import AboutMe from '@/components/main/about-me'
+import Experience from '@/components/main/experience'
+import Hello from '@/components/main/hello'
+import Project from '@components/main/project'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hello />
       <AboutMe />
       <Experience />
+      <Project />
     </main>
-  );
+  )
 }
