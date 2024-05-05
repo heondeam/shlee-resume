049163063e483ffa8fa5e503aa7e5e9b09e61bbe
heondeam <aoao1023@gmail.com>
2024-05-06 00:28:04 +0900

    feat: imple config

---
+++ utils/config.ts
@@ -0,0 +1,24 @@
+export const config = {
+  navLinks: [
+    {
+      name: 'About',
+      link: '#about'
+    },
+    {
+      name: 'Experience',
+      link: '#experience'
+    },
+    {
+      name: 'Projects',
+      link: '#projects'
+    },
+    {
+      name: 'Other Projects',
+      link: '#worthy-projects'
+    },
+    {
+      name: 'Education',
+      link: '#education'
+    }
+  ]
+}
