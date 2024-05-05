31f01ff808cd41e3ce3f6370d63b649ac7bdb62f
heondeam <aoao1023@gmail.com>
2024-05-05 22:10:22 +0900

    feat: imple worthy projects

--- components/typography/h5.tsx
+++ components/typography/h5.tsx
@@ -1,13 +1,13 @@
-import { cn } from "@/utils/utils";
-import { cva } from "class-variance-authority";
+import { cn } from '@/utils/utils'
+import { cva } from 'class-variance-authority'
 
-type H5Props = React.HTMLAttributes<HTMLHeadingElement>;
+type H5Props = React.HTMLAttributes<HTMLHeadingElement>
 
-const h5Variants = cva("whitespace-pre-line text-lg leading-[1.4]");
+const h5Variants = cva('whitespace-pre-line text-lg leading-[1.4] font-semibold')
 
 const H5 = (props: H5Props) => {
-  const { className, ...rest } = props;
-  return <h5 {...rest} className={cn(h5Variants({ className }))} />;
-};
+  const { className, ...rest } = props
+  return <h5 {...rest} className={cn(h5Variants({ className }))} />
+}
 
-export default H5;
+export default H5
