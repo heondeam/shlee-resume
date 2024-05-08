e00f25f5d6501dfb20a19fd019f4339201be987e
heondeam <aoao1023@gmail.com>
2024-05-08 23:10:32 +0900

    feat: imple loading view

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,52 @@
+'use client'
+
+import { useClientWidthHeight } from '@/hooks/useClientWidthHeight'
+import usePreventScroll from '@/hooks/usePreventScroll'
 import Footer from '@components/common/footer'
+import Loading from '@components/common/loading'
+import Navbar from '@components/common/navbar'
+import Sidebar from '@components/common/sidebar'
 import AboutMe from '@components/sections/about-me'
 import Experience from '@components/sections/experience'
 import Hero from '@components/sections/hero'
 import Projects from '@components/sections/projects'
 import WorthyProjects from '@components/sections/worthy-projects'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
+  const [animationFinished, setAnimationFinished] = useState(false)
+  const mainRef = useRef<HTMLDivElement>(null)
+
+  const clientRect = useClientWidthHeight(mainRef)
+  const { width, height } = clientRect
+
+  const handleAnimeEnd = () => {
+    setAnimationFinished(true)
+  }
+
+  usePreventScroll(animationFinished)
+
+  if (!animationFinished) {
+    return (
+      <div className="w-full h-dvh" ref={mainRef}>
+        <Loading canvasWidth={width} canvasHeight={height} onAnimationEnd={handleAnimeEnd} />
+      </div>
+    )
+  }
+
   return (
-    <main>
-      <Hero />
-      <AboutMe />
-      <Experience />
-      <Projects />
-      <WorthyProjects />
-      <Footer />
-    </main>
+    <div>
+      <Navbar />
+      <Sidebar direction="left" />
+      <Sidebar direction="right" />
+      <main>
+        <Hero />
+        <AboutMe />
+        <Experience />
+        <Projects />
+        <WorthyProjects />
+        <Footer />
+      </main>
+    </div>
   )
 }
