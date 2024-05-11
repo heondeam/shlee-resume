c528a627ed04c4b65412db87a09bd25d23233db5
heondeam <aoao1023@gmail.com>
2024-05-11 10:26:23 +0900

    fix: modift styles

--- components/typography/p5.tsx
+++ components/typography/p5.tsx
@@ -3,7 +3,7 @@ import { cva } from 'class-variance-authority'
 
 type P5Props = React.HTMLAttributes<HTMLParagraphElement>
 
-const p5Variants = cva('whitespace-pre-line text-xs md:text-sm font-medium leading-[1.4]')
+const p5Variants = cva('whitespace-pre-line text-sm font-medium leading-[1.4]')
 
 const P5 = (props: P5Props) => {
   const { className, ...rest } = props
